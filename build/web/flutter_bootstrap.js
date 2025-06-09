654593e82696d53391b51058e50e2f516181efd8
eziorobinhood <rrkrish123@gmail.com>
2025-06-09 08:59:36 +0530

    changed background color

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "98460552"
+    serviceWorkerVersion: "1255095665"
   }
 });
